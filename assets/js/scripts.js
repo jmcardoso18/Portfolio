c4be981e1bde0a60798368dabf4d9503b6cb79ca
Jamila Moraes Cardoso <jmc18.ads@gmail.com>
2025-11-10 06:27:09 -0300

    :sparkles:Nova sessao e correção de bug

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -96,5 +96,27 @@ formulario.addEventListener('submit', function(event) {
     
 });
 
+// Inicializar o Swiper.js para o carrossel de projetos
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".projetos_carrossel", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 }
+    }
+  });
+});
+
+
 //chamar a funcao para buscar os dados no github
 getApiGithub();
